5e81c2e27074f67642723bcc4591537a85a5da24
Marianalashta <mariksyharik@gmail.com>
2018-04-20 16:23:10 -0400

    M

--- projectMariana/projectMariana/settings.py
+++ projectMariana/projectMariana/settings.py
@@ -132,8 +132,6 @@ else:
     STATIC_URL = '/static/'
               
     STATIC_ROOT = '/var/www/env/MyDjangoProject_1/projectMariana/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, '/var/www/env/MyDjangoProject_1/projectMariana/static/')
-    ]
+    
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/env/MyDjangoProject_1/projectMariana/media/')
